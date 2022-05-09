8512d971f6b81e185c69068c328ca65ab150b24f
Fangjun.Lu <Fangjun.Lu@geely.com>
2022-05-09 22:21:03 +0800

    second change

--- app/src/main/java/com/lfj/test/MainActivity.kt
+++ app/src/main/java/com/lfj/test/MainActivity.kt
@@ -2,12 +2,14 @@ package com.lfj.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+// comment
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // add 1
-        // add 2
+
+
+
     }
 }
